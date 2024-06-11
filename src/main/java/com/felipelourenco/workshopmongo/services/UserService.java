131e094263353a70e0fcfcfdf1881b2ed48c3610
Felipe Lourenco <lipelourencosilva@gmail.com>
2024-06-11 12:09:46 -0300

    Find user by id method

--- src/main/java/com/felipelourenco/workshopmongo/services/UserService.java
+++ src/main/java/com/felipelourenco/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.felipelourenco.workshopmongo.services;
 
 import com.felipelourenco.workshopmongo.domain.User;
 import com.felipelourenco.workshopmongo.repository.UserRepository;
+import com.felipelourenco.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,14 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        User user = userRepository.findById(id).orElse(null);
+        if(user == null) {
+            throw new ObjectNotFoundException("User with id " + id + " not found");
+        }
+
+        return user;
     }
 }
