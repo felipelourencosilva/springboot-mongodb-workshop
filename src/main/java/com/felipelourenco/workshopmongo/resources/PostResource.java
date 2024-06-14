1e4b85a2b94e55a5459c865a93bb7dc6ee1db8c0
Felipe Lourenco <lipelourencosilva@gmail.com>
2024-06-14 14:02:11 -0300

    Getting Post by id

---
+++ src/main/java/com/felipelourenco/workshopmongo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.felipelourenco.workshopmongo.resources;
+
+import com.felipelourenco.workshopmongo.domain.Post;
+import com.felipelourenco.workshopmongo.domain.User;
+import com.felipelourenco.workshopmongo.dto.UserDTO;
+import com.felipelourenco.workshopmongo.services.PostService;
+import com.felipelourenco.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping(value = "/{id}",method=RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+}
---
+++ src/main/java/com/felipelourenco/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.felipelourenco.workshopmongo.services;
+
+import com.felipelourenco.workshopmongo.domain.Post;
+import com.felipelourenco.workshopmongo.domain.User;
+import com.felipelourenco.workshopmongo.dto.UserDTO;
+import com.felipelourenco.workshopmongo.repository.PostRepository;
+import com.felipelourenco.workshopmongo.repository.UserRepository;
+import com.felipelourenco.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Post post = postRepository.findById(id).orElse(null);
+        if(post == null) {
+            throw new ObjectNotFoundException("User with id " + id + " not found");
+        }
+
+        return post;
+    }
+}
