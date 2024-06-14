0119b0e397c18e0923d6b48ac9306d52291d3282
Felipe Lourenco <lipelourencosilva@gmail.com>
2024-06-14 10:55:38 -0300

    Creating entity Post with nested User

--- src/main/java/com/felipelourenco/workshopmongo/config/Instantiation.java
+++ src/main/java/com/felipelourenco/workshopmongo/config/Instantiation.java
@@ -1,10 +1,17 @@
 package com.felipelourenco.workshopmongo.config;
 
+import com.felipelourenco.workshopmongo.domain.Post;
 import com.felipelourenco.workshopmongo.domain.User;
+import com.felipelourenco.workshopmongo.repository.PostRepository;
 import com.felipelourenco.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -12,15 +19,29 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
+
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
         userRepository.save(maria);
         userRepository.save(alex);
         userRepository.save(bob);
+        postRepository.save(post1);
+        postRepository.save(post2);
     }
 }
